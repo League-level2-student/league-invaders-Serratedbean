30fe0dbda4da4740b2a5f40683d42cfac2d3b422
Student <league@iMac-24.attlocal.net>
2023-06-04 14:34:00 -0700

    rgffsddf
    dsffsfsdsfdsdf

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,7 +4,38 @@ import javax.swing.*;
 
 public class GamePanel extends JPanel{
 
+    final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState = MENU;
 
+    
+    public void updateMenuState() 
+    {  
+
+    	
+    }
+    
+    
+    
+    
+    public void updateGameState() 
+    {  
+
+    	
+    }
+    
+    
+    
+    
+    public void updateEndState()  
+    {  
+
+    	
+    }
+    
+    
+    
 
 	@Override
 	public void paintComponent(Graphics g){
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -4,7 +4,7 @@ public class LeagueInvaders {
 
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
-	
+
 
 
 	
