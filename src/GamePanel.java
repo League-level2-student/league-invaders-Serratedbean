24b3c9695037c8ccd5b745cf7d5f8c8cc1f81371
Student <league@iMac-24.attlocal.net>
2023-06-04 14:30:43 -0700

    rectangle
    raectnagle

---
+++ src/GamePanel.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel{
+
+
+
+	@Override
+	public void paintComponent(Graphics g){
+		
+		g.fillRect(10, 10, 100, 100);
+		
+		
+	}
+}
+
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,39 @@
+import javax.swing.*;
 
 public class LeagueInvaders {
+
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+
+
+	
+	public LeagueInvaders()
+	{
+		setup();
+		
+		
+	}
+	
+	public void setup()
+	{
+		GamePanel game = new GamePanel();
+		JFrame frame = new JFrame();
+		//JPanel panel = new JPanel();
+		frame.setSize(WIDTH, HEIGHT);
+		//panel.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		//frame.add(panel);
+		frame.add(game);
+	}
+	
+	public static void main(String[] args) {
+		
+		LeagueInvaders bugs = new LeagueInvaders();
+		
+		
+
+
+	}
 
 }
